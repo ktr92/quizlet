93b9f5fbf1b7196b4e6894dd76f773a776c261a3
unknown <kudashevtim@gmail.com>
2023-09-23 12:16:15 +0300

    app3

---
+++ android/app/build/intermediates/assets/debug/public/_nuxt/auth.888fdc39.js
@@ -0,0 +1,1 @@
+import{$ as a,g as s,n as t}from"./entry.ff4e4d2d.js";const n=a((e,u)=>{if(!s().value&&e.fullPath!=="/")return t("/login")});export{n as default};
---
+++ android/app/build/intermediates/assets/debug/public/_nuxt/useTailwindConfig.8c6cfb4f.js
@@ -0,0 +1,1 @@
+const n=()=>({defaultTransition:"transition ease-in-out duration-350"});export{n as u};
