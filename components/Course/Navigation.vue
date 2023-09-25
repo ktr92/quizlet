85a578c843fe38d5b43aadc2e323b13cba83a465
unknown <kudashevtim@gmail.com>
2023-09-25 21:17:00 +0300

    progress color

--- components/Course/Navigation.vue
+++ components/Course/Navigation.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
- 
     <div class="flex max-auto my-8 md:my-8 justify-center">
-      
       <div class="mx-2 md:mx-4" v-if="props.condition1">
         <UIButton
           @click="$emit('someEvent1')"
@@ -26,23 +24,26 @@
         </UIButton>
       </div>
     </div>
-        <div>
-          <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 my-12">
-            <div class="bg-[#423ed8] text-md font-medium text-blue-100 text-center p-2 leading-none rounded-full" :style='"width: " + barWidthCalculated + "%"'> {{ barWidthCalculated }} %</div>
-          </div>
-
-          <div v-if="falseItems?.length">
-            <h3 class="text-xl font-bold text-red-400">Wrong answers</h3>
-          <UITable :items='falseItems'></UITable>
-          </div> <div v-if="trueItems?.length">
-            <h3 class="text-xl font-bold text-[#08BB3C]">Right answers</h3>
-          <UITable :items='trueItems'></UITable>
-          </div>
-         
-       
-         
-      
+    <div>
+      <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 my-12">
+        <div
+          class="bg-[#423ed8] min-w-[50px] text-md font-medium text-blue-100 text-center p-2 leading-none rounded-full"
+          :class="barColor"
+          :style="'width: ' + barWidthCalculated + '%'"
+        >
+          {{ barWidthCalculated }} %
+        </div>
       </div>
+
+      <div v-if="falseItems?.length">
+        <h3 class="text-xl font-bold text-red-400">Wrong answers</h3>
+        <UITable :items="falseItems"></UITable>
+      </div>
+      <div v-if="trueItems?.length">
+        <h3 class="text-xl font-bold text-[#08BB3C]">Right answers</h3>
+        <UITable :items="trueItems"></UITable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -66,22 +67,39 @@ const props = defineProps({
   },
   trueItems: {
     type: Array<IWord>,
-    required: false
+    required: false,
   },
   falseItems: {
     type: Array<IWord>,
-    required: false
+    required: false,
   },
   count: {
     type: Number,
     default: 0,
-  }
+  },
 })
 
 const barWidthCalculated = computed(() => {
-  return props.falseItems ? (props.count - props.falseItems.length) / props.count * 100 : 0
+  return props.falseItems
+    ? ((props.count - props.falseItems.length) / props.count) * 100
+    : 0
 })
 
+const barColor = computed(() => {
+  if (barWidthCalculated.value <= 25) {
+    return "bg-red-400 text-white"
+  }
+  if (barWidthCalculated.value > 25 && barWidthCalculated.value < 50) {
+    return "bg-orange-400 text-blue-800"
+  }
+  if (barWidthCalculated.value >= 50 && barWidthCalculated.value < 80) {
+    return "bg-yellow-400 text-blue-800"
+  }
+  if (barWidthCalculated.value >= 80) {
+    return "bg-green-500 text-white"
+  }
+  return "bg-[#423ed8]"
+})
 </script>
 
 <style scoped></style>
