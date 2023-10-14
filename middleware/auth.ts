ff0192332128a2f17817eb98ab0c5606397c48f5
unknown <kudashevtim@gmail.com>
2023-10-14 13:28:27 +0300

    redorectTo works

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const user = useSupabaseUser()
+ /*  const user = useSupabaseUser()
 
   if (!user.value && to.fullPath !== '/') {
     return navigateTo('/login')
-  } 
+  }  */
 })
